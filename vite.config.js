bdca5f08f0ff819857a91c0835ab7994b80e5381
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-08-15 20:19:47 +0330

    add title

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-   base: '/Calculate-Tip/',
+  plugins: [react(), tailwindcss()],
+  base: '/Calculate-Tip/', // حتماً همین شکل (حساس به بزرگ/کوچک بودن حروف)
 })
